f527b866bd632df62f84e5f55880d59929ab0efb
Elliott Landsborough <elliott.landsborough@gmail.com>
2023-01-05 02:27:53 +0000

    Add some headers

--- src/libraries/response.ts
+++ src/libraries/response.ts
@@ -1,14 +1,21 @@
+const headers = {
+	'content-type': 'application/json;charset=UTF-8',
+	'access-control-allow-origin': '*',
+	// Cache for 1 hour
+	'cache-control': 's-maxage=3600, max-age=3600',
+}
+
 export function responseTwoDimensional(data: { [k: string]: string[]; }): Response {
 	const json = JSON.stringify({'status': 'success', 'message': data});
-	return new Response(json, {headers: {'content-type': 'application/json;charset=UTF-8'}})
+	return new Response(json, {headers: headers})
 }
 
 export function responseOneDimensional(data: string[]): Response  {
 	const json = JSON.stringify({'status': 'success', 'message': data});
-	return new Response(json, {headers: {'content-type': 'application/json;charset=UTF-8'}})
+	return new Response(json, {headers: headers})
 }
 
 export function responseString(data: string): Response  {
 	const json = JSON.stringify({'status': 'success', 'message': data});
-	return new Response(json, {headers: {'content-type': 'application/json;charset=UTF-8'}})
+	return new Response(json, {headers: headers})
 }
