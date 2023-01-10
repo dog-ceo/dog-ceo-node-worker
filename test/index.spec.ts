88ff947c43896603d0ba08c21f32cbc74198823f
Elliott Landsborough <elliott.landsborough@gmail.com>
2023-01-10 05:37:39 +0000

    Mock s3 bucket

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,4 +1,5 @@
 import { handleRequest } from "@/index";
+import AWSMock from "mock-aws-s3";
 
 test("should send 404", async () => {
   const env = getMiniflareBindings();
@@ -9,6 +10,18 @@ test("should send 404", async () => {
 
 test("should get list of all breeds", async () => {
   const env = getMiniflareBindings();
+
+  AWSMock.config.basePath = './.tmp/buckets/' // Can configure a basePath for your local buckets
+  var s3 = new AWSMock.S3({
+    params: { Bucket: env.R2_BUCKET }
+  });
+
+  s3.putObject({Key: 'breeds/hound/image1.jpg', Body: '12345', Bucket: env.R2_BUCKET}, function(err: any, data: any) {
+    s3.listObjects({Prefix: 'breeds/hound', Bucket: env.R2_BUCKET}, function (err: any, data: any) {
+      console.log(data);
+    });
+  });
+
   const res = await handleRequest(new Request("http://localhost/api/breeds/list/all"), env);
   expect(res.status).toBe(404);
 });
