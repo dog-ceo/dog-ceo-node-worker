f34a111f1aa8b01eacd04ae339be9ae95592998a
Elliott Landsborough <elliott.landsborough@gmail.com>
2023-01-10 01:01:06 +0000

    Almost working

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,26 @@
 export default {
-    testEnvironment: "miniflare",
-    // Configuration is automatically loaded from `.env`, `package.json` and
-    // `wrangler.toml` files by default, but you can pass any additional Miniflare
-    // API options here:
     testEnvironmentOptions: {
-        bindings: { KEY: "value" },
-        kvNamespaces: ["TEST_NAMESPACE"],
+        // Miniflare doesn't yet support the `main` field in `wrangler.toml` so we
+        // need to explicitly tell it where our built worker is. We also need to
+        // explicitly mark it as an ES module.
+        //scriptPath: "dist/index.mjs",
+        //modules: true,
+        //bindings: { KEY: "value" },
+        //kvNamespaces: ["TEST_NAMESPACE"],
     },
     collectCoverageFrom: [
         "index.js",
         "src/*.js"
-    ]
+    ],
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/src/$1",
+        "^(\\.{1,2}/.*)\\.js$": "$1",
+    },
+    preset: "ts-jest/presets/default-esm",
+    globals: {
+        "ts-jest": {
+            tsconfig: "test/tsconfig.json",
+            useESM: true,
+        },
+    },
 };
---
+++ test/index.spec.ts
@@ -0,0 +1,7 @@
+import { handleRequest } from "@/index";
+
+test("should redirect to example page on no route match", async () => {
+  const env = getMiniflareBindings();
+  const res = await handleRequest(new Request("http://localhost"), env);
+  expect(res.status).toBe(404);
+});
