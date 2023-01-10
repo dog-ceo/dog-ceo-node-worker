d09619b70c5fcd080b36829b9c286f5a7ce5d300
Elliott Landsborough <elliott.landsborough@gmail.com>
2023-01-10 01:42:05 +0000

    Fix dependency issue

--- jest.config.ts
+++ jest.config.ts
@@ -16,6 +16,8 @@ export default {
     moduleNameMapper: {
         "^@/(.*)$": "<rootDir>/src/$1",
         "^(\\.{1,2}/.*)\\.js$": "$1",
+        // https://stackoverflow.com/questions/73203367/jest-syntaxerror-unexpected-token-export-with-uuid-library
+        "^uuid$": "uuid"
     },
     preset: "ts-jest/presets/default-esm",
     globals: {
